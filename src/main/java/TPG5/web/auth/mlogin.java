95c3a8c66f1cf22e828a93563830d94901a48ac4
appleipod7 <49970635+appleipod7@users.noreply.github.com>
2022-11-03 16:17:21 -0400

    fix mlogin parameters

--- src/main/java/TPG5/web/auth/mlogin.java
+++ src/main/java/TPG5/web/auth/mlogin.java
@@ -2,8 +2,10 @@ package TPG5.web.auth;
 import java.io.IOException;
 import java.util.ArrayList;
 import TPG5.web.model.mRegistration;
+import TPG5.web.scrape.scrapedataObject;
 import TPG5.web.dataObject.dataObject;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -12,22 +14,22 @@ public class mlogin extends HttpServlet {
 	mRegistration mLoginFields = new mRegistration();
 	private static final long serialVersionUID = 1L;
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		String username= req.getParameter("txtusr_name");
-		String password= req.getParameter("txtusr_pwd");
+		String username= req.getParameter("username");
+		String password= req.getParameter("password");
 		mLoginFields.setusrname(username);
 		mLoginFields.setpassword(password);
-		ArrayList user_login = new dataObject().member_register(username, password);
+		ArrayList user_login = new dataObject().member_login(username, password);
 		if(user_login.size()>0)
 		{
-			String user_fname = user_login.get(0).toString();
-			String user_lname = user_login.get(1).toString();
-			String user_laccess = user_login.get(2).toString();
+			String usr_fname = user_login.get(0).toString();
+			String usr_lname = user_login.get(1).toString();
+			String usr_laccess = user_login.get(2).toString();
 			HttpSession session = req.getSession();
 			
 			session.setAttribute("usr_fname", usr_fname);
 			session.setAttribute("usr_lname", usr_lname);
 			session.setAttribute("usr_laccess", usr_laccess);
-			scrapeDataObj update_scrape_tbl = new scrapeDataObj();
+			scrapedataObject update_scrape_tbl = new scrapedataObject();
 			update_scrape_tbl.get_web_data();
 			res.sendRedirect("site_pages/members.jsp");
 		}
