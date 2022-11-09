7f2b9c769aba5578c53e721d747ba58b6b46b767
appleipod7 <49970635+appleipod7@users.noreply.github.com>
2022-11-09 03:30:50 -0500

    Added mlogout

--- src/main/java/TPG5/web/auth/mlogout.java
+++ src/main/java/TPG5/web/auth/mlogout.java
@@ -1,1 +1,20 @@
 package TPG5.web.auth;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class mlogout extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("usr_fname");
+        session.removeAttribute("usr_lname");
+        session.removeAttribute("usr_laccess");
+        session.invalidate();
+        response.sendRedirect("index.jsp");
+    }
+}
