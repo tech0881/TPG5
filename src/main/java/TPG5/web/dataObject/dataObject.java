ef8d10948d136a4fa2f2a60d69027d42268e0e5c
appleipod7 <49970635+appleipod7@users.noreply.github.com>
2022-11-09 04:02:34 -0500

    dataObject Progress 2

--- src/main/java/TPG5/web/dataObject/dataObject.java
+++ src/main/java/TPG5/web/dataObject/dataObject.java
@@ -18,7 +18,7 @@ import TPG5.web.model.mRegistration;
 public class dataObject {
     dbConnect dbc = new dbConnect("tpg5");
     mRegistration mR = new mRegistration();
-    Connection conn = dbc.connection_string();
+    Connection conn = dbc.str_connection();
     Statement stmt = null;
     ResultSet rs;
     HttpServletResponse resp;
@@ -29,14 +29,14 @@ public class dataObject {
     String userAccessLvl = null;
     
     public mRegistration member_register(String fname, String mname, String lname, String usrname, 
-    String password, String rootpass, String email, String useraccess, String oboSq1, String oboSql2,
+    String password, String rootpass, String email, String useraccess, String oboSq1, String oboSq2,
     String txtans1, String txtans2) {
 
         firstN = mR.getFname();
         lastN = mR.getlname();
         userAccessLvl = mR.getuseraccess();
         String objoboSql = mR.getoboSq1();
-        String objoboSql2 = mR.getoboSql2();
+        String objoboSql2 = mR.getoboSq2();
         String objTxtans1 = mR.gettxtans1(); 
         String objTxtans2 = mR.gettxtans2(); 
 
@@ -63,14 +63,14 @@ public class dataObject {
         String qry_user = "SELECT * FROM users";
         try
         {
-            String hash_val_pwd = conn.getSHA_pwd(pwd);
+            String hash_val_pwd = dbc.getSHA_pwd(password);
             stmt = conn.createStatement();
             rs = stmt.executeQuery(qry_user);
             while(rs.next())
             {
                 n_of_u_found++;
 
-                if(rs.getString("u_name").equals(uname))
+                if(rs.getString("u_name").equals(usrname))
                 {
                     usrExist = true;
                     RequestDispather rd = new RequestDispather();
@@ -79,23 +79,59 @@ public class dataObject {
                 }
             }
             if(n_of_u_found==0) {
-                if(pwd.equals(rpwd)){
+                if(password.equals(rootpass)){
                     String insert_db = "INSERT INTO users (f_name, m_name, u_name, u_pwd, u_email, u_access, u_sq1, u_ans1, u_sq2, u_ans2, u_ref_id, u_reg_date, u_exp_data, acct_status)";
                     PreparedStatement pstmt = conn.prepareStatement(insert_db);
                     pstmt.setString(1, "Administrator");
                     pstmt.setString(2, "A");
                     pstmt.setString(3, "Account");
                     pstmt.setString(4, "admin");
-                    pstmt.setString(5, hash);
-                    pstmt.setString(6, "");
-                    pstmt.setString(7, "");
-                    pstmt.setString(8, "");
+                    pstmt.setString(5, hash_val_pwd);
+                    pstmt.setString(6, "admin@company.com");
+                    pstmt.setString(7, "alway_active");
+                    pstmt.setString(8, oboSq1);
+                    pstmt.setString(9, txtans1);
+                    pstmt.setString(10, oboSq2);
+                    pstmt.setString(11, txtans2);
+                    pstmt.setInt(12, 1000);
+                    pstmt.setString(13, strcurrent_date);
+                    pstmt.setString(14, stneverdt);
+                    pstmt.setString(15, acc_status[0]);
+
+                    pstmt.execute();
                 }
             }
+            else {
+                if(password.equals(rootpass)) {
+                    String insert_db = "INSERT INTO users (f_name, m_name, u_name, u_pwd, u_email, u_access, u_sq1, u_ans1, u_sq2, u_ans2, u_ref_id, u_reg_date, u_exp_data, acct_status)";
+                    PreparedStatement pstmt = conn.prepareStatement(insert_db);
 
-        }
+                    pstmt.setString(1, fname);
+                    pstmt.setString(2, mname);
+                    pstmt.setString(3, lname);
+                    pstmt.setString(4, usrname);
+                    pstmt.setString(5, hash_val_pwd);
+                    pstmt.setString(6, email);
+                    pstmt.setString(7, useraccess);
+                    pstmt.setString(8, oboSq1);
+                    pstmt.setString(9, txtans1);
+                    pstmt.setString(10, oboSq2);
+                    pstmt.setString(11, txtans2);
+                    pstmt.setInt(12, 8555555);
+                    pstmt.setString(13, strcurrent_date);
+                    pstmt.setString(14, stneverdt);
+                    pstmt.setString(15, acc_status[1]);
 
-        return m;
+                    pstmt.executeUpdate();
+                }
+            }
+        }            
+        catch(Exception e) {
+                System.out.printIn(e);
+                System.out.println("Wrong");
+            }
+
+        return mR;
     }
     
     public ArrayList member_login(String username, String password) {
@@ -111,6 +147,15 @@ public class dataObject {
                 String usern = rs.getString("u_name").toString();
                 String usere = rs.getString("u_email").toString();
                 String userp = rs.getString("u_pwd").toString();
+                if (usern.equals(username) || usere.equals(username){
+                   if(userp.equals(hash_val_pwd)) {
+
+                   }
+                }
+                else {
+                    
+                }
+                
             }
         }
     }
